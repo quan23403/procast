3486dbe6a17e584cb1adb41e96e250c0ddd9d3b6
locnguyen3108 <loclieulinh318@gmail.com>
2023-09-12 11:52:47 +0700

    setup login

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import useRouteElements from './useRouteElement'
+
+function App() {
+  const routeElements = useRouteElements()
+  return <div>{routeElements}</div>
+}
+
+export default App
