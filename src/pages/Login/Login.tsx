54a3d0489b4554105ed3dd565eac17280f11fdee
CaoThanhNg03 <cthanhnguyen03@gmail.com>
2023-12-27 00:00:50 +0700

    x

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -125,7 +125,7 @@ export default function Login() {
               </div>
               <Button
                 type='submit'
-                className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex justify-center items-center'
+                className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex justify-center items-center'
                 isLoading={loginMutation.isLoading}
                 disabled={loginMutation.isLoading}
               >
